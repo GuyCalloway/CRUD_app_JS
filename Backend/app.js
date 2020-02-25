932d5128c862a0894e96d4b4284516d009d5492e
GuyCalloway <guyfcalloway@gmail.com>
2020-02-25 10:40:28 +0000

    Set-up: Basic CRUD functionality achieved and tested for

--- Backend/app.js
+++ Backend/app.js
@@ -2,5 +2,29 @@ class MessageApp {
   constructor() {
     this.messages = []
   }
+  // C
+  post(content) {
+    let item = {
+      id: this.messages.length,
+      content: content,
+      date: new Date()
+    }
+    this.messages.push(item)
+    return this.messages
+  }
+  // R
+  get(id) {
+    return this.messages[id];
+  }
+  // U
+  update(id, update) {
+    this.messages[id].content = update
+    return this.messages[id]
+  }
+  // D
+  delete(id) {
+    this.messages.splice(id - 1, 1)
+    return this.messages
+  }
 }
 export default MessageApp
